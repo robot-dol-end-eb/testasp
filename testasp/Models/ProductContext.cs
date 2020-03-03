8c8d4d4cd003f65b4d548b3ec9e69eb5f00c5779
robot-dol-and-eb <bikoluso@gmail.com>
2020-03-03 18:23:31 +0300

    second commit

---
+++ testasp/Models/ProductContext.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.Entity;
+
+namespace testasp.Models
+{
+    public class ProductContext : DbContext
+    {
+        public ProductContext() : base("testasp")
+        {
+        }
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Product> Products { get; set; }
+    }
+}
