380085e0827cb8a65dfdf099cd64016dbb7f0d51
robot-dol-and-eb <bikoluso@gmail.com>
2020-03-03 23:54:46 +0300

    thread commit

--- testasp/Site.Master.cs
+++ testasp/Site.Master.cs
@@ -7,6 +7,8 @@ using System.Web.Security;
 using System.Web.UI;
 using System.Web.UI.WebControls;
 using Microsoft.AspNet.Identity;
+using System.Linq;
+using testasp.Models;
 
 namespace testasp
 {
@@ -71,7 +73,12 @@ namespace testasp
         {
 
         }
-
+        public IQueryable<Category> GetCategories()
+        {
+            var _db = new testasp.Models.ProductContext();
+            IQueryable<Category> query = _db.Categories;
+            return query;
+        }
         protected void Unnamed_LoggingOut(object sender, LoginCancelEventArgs e)
         {
             Context.GetOwinContext().Authentication.SignOut(DefaultAuthenticationTypes.ApplicationCookie);
